0934aa179cd08b7736896bec77c4cff9589b71ce
norliana14091975-maker <norliana14091975@gmail.com>
2026-08-26 00:56:34 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  serverExternalPackages: ["@napi-rs/canvas"],
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
